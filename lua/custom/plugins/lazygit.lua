be1bcff52d1061097224dca987a34da173538af2
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2023-08-01 20:53:36 -0300

    add little basic config
    its so nice being able to do descriptions

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -4,4 +4,10 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  config = function()
+    vim.g.lazygit_floating_window_scaling_factor = 0.8
+    vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
+    require("telescope").load_extension("lazygit")
+    vim.keymap.set("n", "<leader>gt", ":lua require('telescope').extensions.lazygit.lazygit()<CR>")
+  end
 }
