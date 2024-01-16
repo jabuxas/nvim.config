348cd048b5cd64b7bc5908e7e91fd61f46379272
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-01-16 12:52:40 -0300

    add image support

---
+++ lua/custom/plugins/image.lua
@@ -0,0 +1,12 @@
+return {
+  "3rd/image.nvim",
+  config = function()
+    package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
+    package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
+
+    require("image").setup({
+      backend = "ueberzug",
+      tmux_show_only_in_active_indow = true,
+    })
+  end,
+}
