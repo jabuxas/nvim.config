5e147aec68d00505341d81d38d3ec0a44ed6f73f
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-08-20 16:20:20 -0300

    fix: remove default browser
    i dont use qutebrowser anymore

--- lua/custom/plugins/md.lua
+++ lua/custom/plugins/md.lua
@@ -3,7 +3,4 @@ return {
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
   build = function() vim.fn["mkdp#util#install"]() end,
-  config = function()
-    vim.g.mkdp_browser = '/usr/bin/qutebrowser'
-  end
 }
